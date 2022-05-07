0a10e045926f0b4be597a9ec22b55db5adcb4a3f
themasterz854 <62380365+themasterz854@users.noreply.github.com>
2022-05-07 19:26:41 +0530

    react started

--- fileserver.js
+++ fileserver.js
@@ -4,23 +4,17 @@ const https = require('https');
 const privateKey = fs.readFileSync('./key.pem', 'utf8');
 const certificate = fs.readFileSync('./cert.pem', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
-
 absolutepathofhtml = __dirname + "/html/";
-
 const app = express();
-
+const httpsServer = https.createServer(credentials, app);
 
 app.get("/",(req,res) => {
 
     res.sendFile(absolutepathofhtml+"home.html");
 });
+app.get("/downloadfile",(req,res) => {
+ res.download(__dirname + "/SSBVEGITO.png");
+});
 httpsServer.listen(8443);
-
-
-
-
-
-
-
-
 console.log("Server listening https on port 8443");
+
